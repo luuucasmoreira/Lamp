51d8abf28c06fb4aeeab5bf8ef4e1ab195724678
luuucasmoreira <luuucasmoreira1994@gmail.com>
2020-04-09 16:35:25 -0300

    Acertando detalhes do Firebase

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const { Board, Led, Relay } = require("johnny-five");
-var firebase = require("firebase");
+const { Board, Led, Relay } = require("johnny-five"); //Chamando biblioteca para rodar os componentes do arduino
+var firebase = require("firebase"); //conexão com o banco de dados.
 
-const board = new Board();
+const board = new Board(); 
 
 board.on("ready", () => {
   
   const rele = new Relay(8); 
+  const led = new Led(13);
 
   board.repl.inject({
-    
+    led,
     rele
   });
 
  
+ //------Lendo a mudança do banco------
+  
   var config = {
     apiKey: "AIzaSyDaltCU5hPZFEXNZreIQkQbKBS_-0Vqadg",
     authDomain: "iot-lamp-cd664.firebaseapp.com",
@@ -23,7 +26,8 @@ board.on("ready", () => {
 
   firebase.initializeApp(config);
 
-  
+  // vareaveu para buscar no banco a referencia do campo lampada o valor atribuido
+  //e entao demontra o estado de on ou off
 
   var starCountRef = firebase.database().ref('lampada').on('value', function(snapshot) {
   
@@ -31,8 +35,10 @@ board.on("ready", () => {
 
     if (lampada == 'on') {
         rele.on()
+        
     } else {
         rele.off()
+        
     }
     
 });
